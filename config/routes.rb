ee1142a322a7597940d0d2d43f4d0813e3dd69d4
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-05-06 12:19:08 +0000

    [Add]ジャンル一覧・追加画面

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   }
   namespace :admin do
     get '/' => 'homes#top'
+    resources :genres, only:[:index, :create, :edit, :update]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
