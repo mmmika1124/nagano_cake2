9f25c436444fbd482caaa33b5065194e2c1c47e8
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-05-18 09:37:47 +0000

    [Add]商品詳細画面

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.all
+    @genres = Genre.all
   end
 
   def show
+    @item = Item.find(params[:id])
+    @genres = Genre.all
   end
 end
