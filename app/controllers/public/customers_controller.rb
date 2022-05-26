e526d5ab23b428afac7233917864dbf3a95c1330
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-05-26 11:43:44 +0000

    [Add]会員情報編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,5 +4,22 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = current_customer
   end
+
+  def update
+    customer = current_customer
+    if customer.update(customer_params)
+      redirect_to customers_mypage_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_active)
+  end
+
 end
