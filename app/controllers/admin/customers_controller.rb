e07d576bdcab7a7ad15af246f610751471275b10
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-05-29 07:44:39 +0000

    [Add]会員詳細画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,9 +4,11 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   private
