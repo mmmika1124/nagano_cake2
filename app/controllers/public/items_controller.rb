484235784018b7696df3779cdcdb15dc71447130
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-05-31 03:48:25 +0000

    [Update]トップページ/商品一覧画面

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,13 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.where(is_active: :true)
     @genres = Genre.all
+    if params[:genre_id]
+      @genre = @genres.find(params[:genre_id])
+      @all_items = @genre.items.where(is_active: :true)
+    else
+      @all_items = Item.where(is_active: :true).includes(:genre)
+    end
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,8 @@ class Item < ApplicationRecord
   attachment :image
   belongs_to :genre
 
-  validates :genre_id, presence: true
   validates :name, presence: true
   validates :introduction, presence: true
   validates :price, presence: true
-  validates :is_active, presence: true
 
 end
