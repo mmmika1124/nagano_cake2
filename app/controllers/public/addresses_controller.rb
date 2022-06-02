1323c7ecb2a61674ce7ff31b6a0b71d3501dac7f
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-06-02 03:44:21 +0000

    [Add]配送先登録・一覧画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,33 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = current_customer.addresses
+  end
+
+  def create
+    address = Address.new(address_params)
+    address.customer_id = current_customer.id
+    if address.save
+      redirect_to addresses_path
+    else
+      @addresses = current_customer.addresses
+      render :index
+    end
   end
 
   def edit
   end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
+  end
+
 end
