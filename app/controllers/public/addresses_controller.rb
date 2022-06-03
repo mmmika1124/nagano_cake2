605c2a69540e440e6463263be79f13e6600df32d
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-06-03 02:33:00 +0000

    [Add]配送先編集画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -16,12 +16,26 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    address = Address.find(params[:id])
+    if address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    address = Address.find(params[:id])
+    if address.destroy
+      redirect_to addresses_path
+    else
+      @addresses = current_customer.addresses
+      render :index
+    end
   end
 
   private
