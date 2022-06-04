244bac2ca35f7e221ec0782c47819dfdb93cd3a1
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-06-04 02:45:27 +0000

    [Update]退会画面

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :customer_state, only: [:create]
 
   def after_sign_in_path_for(resource)
     root_path
@@ -9,6 +10,16 @@ class Public::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource)
     root_path
   end
+
+  protected
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    if @customer.valid_password?(params[:customer][:password]) && @customer.is_active == false
+      redirect_to new_customer_registration_path
+    end
+  end
+
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
