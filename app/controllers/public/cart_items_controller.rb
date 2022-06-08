82dca014938120a7327207cb8dce8a968920ab7f
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-06-08 12:34:53 +0000

    [Update]カート画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,6 +3,7 @@ class Public::CartItemsController < ApplicationController
     cart_item = CartItem.new(cart_item_params)
     cart_item.customer = current_customer
     if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
       cart_item.amount += params[:cart_item][:amount].to_i
       cart_item.save
         redirect_to cart_items_path
@@ -17,6 +18,7 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
+    @total = @cart_items.inject(0){|total,cart_item|total + cart_item.subtotal}
   end
 
   def update
