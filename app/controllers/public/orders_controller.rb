07f38527f016cb4a7328ffae1c4f171c76d24623
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-06-09 10:08:35 +0000

    [Update]注文情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,6 +6,21 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+    if params[:order][:select_address] == 0
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:select_address] == 1
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    elsif params[:order][:select_address] == 2
+      @order.postal_code = order_params[:postal_code]
+      @order.address = order_params[:address]
+      @order.name = order_params[:name]
+    end
   end
 
   def create
@@ -20,7 +35,7 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:cart_item).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method, :status)
   end
 
 end
