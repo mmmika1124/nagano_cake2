115cebbd540157c2df7c18edcec6f39b49f571e9
mmmika1124 <96968528+mmmika1124@users.noreply.github.com>
2022-06-23 03:39:42 +0000

    [Add]注文履歴詳細画面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,18 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
   end
+
+  def update
+    order = Order.find(params[:id])
+    order.update(status: params[:order][:status])
+    redirect_to admin_order_path
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
